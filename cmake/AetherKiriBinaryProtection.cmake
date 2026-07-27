7bfb17d6111cf9e6013c20aaffea26c17d537df6
kyoiryi <railgunf@outlook.com>
2026-07-27 16:18:10 +0800

    build(protection): link Kagura target runtime

--- cmake/AetherKiriBinaryProtection.cmake
+++ cmake/AetherKiriBinaryProtection.cmake
@@ -3,6 +3,29 @@ include_guard(GLOBAL)
 set(AETHERKIRI_OBFUSCATION_POLICY
     "${CMAKE_CURRENT_LIST_DIR}/internal_obfuscation.json")
 
+function(aetherkiri_link_kagura_runtime target_name)
+    if(NOT EXISTS "${AETHERKIRI_KAGURA_SOURCE_DIR}/runtime/CMakeLists.txt" OR
+       NOT EXISTS "${AETHERKIRI_KAGURA_SOURCE_DIR}/include/kagura/runtime.h")
+        message(FATAL_ERROR
+            "Kagura runtime sources are missing: "
+            "${AETHERKIRI_KAGURA_SOURCE_DIR}")
+    endif()
+
+    if(NOT TARGET kagura_runtime)
+        add_subdirectory(
+            "${AETHERKIRI_KAGURA_SOURCE_DIR}/runtime"
+            "${CMAKE_BINARY_DIR}/kagura-runtime"
+            EXCLUDE_FROM_ALL)
+        target_include_directories(kagura_runtime PUBLIC
+            "${AETHERKIRI_KAGURA_SOURCE_DIR}/include")
+        if(ANDROID OR (UNIX AND NOT APPLE))
+            target_link_libraries(kagura_runtime PUBLIC ${CMAKE_DL_LIBS})
+        endif()
+    endif()
+
+    target_link_libraries("${target_name}" PRIVATE kagura_runtime)
+endfunction()
+
 function(aetherkiri_protect_internal_sources target_name)
     if(NOT AETHERKIRI_ENABLE_CODE_OBFUSCATION)
         return()
@@ -38,6 +61,8 @@ function(aetherkiri_protect_internal_sources target_name)
          obfuscator_plugin)
     file(TO_CMAKE_PATH "${AETHERKIRI_OBFUSCATION_POLICY}"
          obfuscation_policy)
+
+    aetherkiri_link_kagura_runtime("${target_name}")
 
     foreach(internal_source IN LISTS ARGN)
         if(NOT EXISTS "${internal_source}")
